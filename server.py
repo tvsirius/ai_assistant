e753b2ccbe64fa2cf235966892bf2107916cbaee
Denis Savitsky <densav@gmail.com>
2023-05-18 20:05:09 +0300

    v0.8 - base task done, AI assistant using ConversationalSummaryBuffer memory with persistent storage

--- server.py
+++ server.py
@@ -19,6 +19,7 @@ import chromadb
 
 from langchain.memory import ConversationBufferMemory, ChatMessageHistory, ConversationSummaryBufferMemory
 from langchain import OpenAI, PromptTemplate, LLMChain, ConversationChain
+# from langchain.prompts import ChatPromptTemplate
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
@@ -45,16 +46,19 @@ chat = ChatOpenAI(temperature=0.6, model_name="gpt-3.5-turbo",
 embedding = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
 prompt = PromptTemplate(
-    input_variables=["history", "human_input"],
+    input_variables=["history","human_input"],
     template=template
 )
 
 conversation = LLMChain(
     llm=chat,
     verbose=True,
-    memory=ConversationSummaryBufferMemory(llm=chat),
+    memory=ConversationSummaryBufferMemory(llm=chat,  max_token_limit=1000, return_messages=True),
     prompt=prompt,
 )
+
+
+
 
 embeddings = None
 # embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY, model="text-embedding-ada-002")
@@ -87,13 +91,12 @@ def load_from_vectorstore():
     read_history = vectorstore._collection.get(ids=[CHROMA_ID_FULL_JSON], include=["documents"])
     print(read_history)
     if len(read_history['documents']) > 0 and len(read_history['documents'][0]) > 0:
-        return read_history['documents'][0]
+        return json.loads(read_history['documents'][0])
 
 
 def save_to_vectorstore(ids, text):
-    vectorstore._collection.upsert(ids=[ids], documents=[text])
+    vectorstore._collection.upsert(ids=[ids], documents=[json.dumps(text)])
     vectorstore.persist()
-
 
 def load_history():
     history_load = load_from_vectorstore()
@@ -103,33 +106,57 @@ def load_history():
         return ''
 
 
-history_text = load_history()
-print('history_text=', history_text)
+history_dict = load_history()
+history_messages=messages_from_dict(history_dict)
+# history_text = load_history()
+history_text = str(history_dict)
+print('history_text=', history_dict)
+print('history_messages=', history_messages)
+
+if history_messages:
+    conversation.memory.chat_memory.messages=history_messages
+    conversation.memory.predict_new_summary(history_messages, '')
+
+
+print(conversation.memory.load_memory_variables({}))
+print(conversation.memory.buffer)
+
+
+
 
 
 @server.route("/", methods=("GET", "POST"))
 def index():
-    global history_text
+    def primitive_formatter(message_list: list[dict]) -> str:
+        result = ''
+        for message in message_list:
+            result += '<b>' + message["type"].capitalize() + ': </b>' + message["data"]["content"] + "<br>"
+        return result
+    global history_text, history_dict
     if request.method == "POST":
         human_text = request.form["text"]
         if human_text == 'clear history':
             history_text = ''
             conversation.memory.clear()
         else:
-            print(f'sending req. hyman text={human_text}, history_text={history_text}')
-            response = conversation.predict(human_input=human_text, history=history_text)
+            print(f'sending req. hyman text={human_text}')
+            response = conversation.predict(human_input=human_text, history='')
             print(f'response={response}')
+        history_dict=messages_to_dict(conversation.memory.load_memory_variables({})["history"])
         print(
-            f'conversation.memory.load_memory_variables()["history"]={conversation.memory.load_memory_variables({})["history"]}')
-        history_text = conversation.memory.load_memory_variables({})['history']
-        save_to_vectorstore(CHROMA_ID_FULL_JSON, history_text)
-        return redirect(url_for("index", result=history_text))
+            f'conversation.memory.load_memory_variables()["history"]={history_dict}')
+        history_text = str(history_dict)
+        save_to_vectorstore(CHROMA_ID_FULL_JSON, history_dict)
+        return redirect(url_for("index", last_response=response))
 
     # history = conversation.memory.load_memory_variables({})['history']
     # print(history_text)
     # print(load_from_vectorstore())
+    get_last_response=request.args.get('last_response')
+    if get_last_response and len(get_last_response)>0 and get_last_response!=history_dict[-1]["data"]["content"]:
+        return render_template("index.html", result=primitive_formatter(history_dict+[{'type': 'ai', 'data': {'content': get_last_response}}]))
 
-    return render_template("index.html", result=history_text)
+    return render_template("index.html", result=primitive_formatter(history_dict))
 
 
 
